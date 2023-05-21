e43c64159f0fa2a177c45899233a6bdbc926a9b4
sparshtiwari10 <sparsh8188811011@gmail.com>
2023-05-22 01:09:53 +0530

    trying read-excel

--- exc.js
+++ exc.js
@@ -1,5 +1,10 @@
 
-const workBook= XLSX.readFile("ala.xlsx")
-const workSheet = workBook.Sheets("Table 1")
-const channels=XLSX.utils.sheet_to_json(workSheet)
-console.log(channels)
+// //const XLSX=require("xlsx")
+// const workBook= XLSX.readFile("ala.xlsx")
+// const workSheet = workBook.Sheets("Table 1")
+// const channels=XLSX.utils.sheet_to_json(workSheet)
+// console.log(channels)
+
+readXlsxFile("ala.xlsx").then((rows) => {
+    console.log(rows)
+})
