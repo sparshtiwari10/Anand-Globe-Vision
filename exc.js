2d9a481ee3c20a85f7b8ab3497c12420596c6a14
sparshtiwari10 <57325189+sparshtiwari10@users.noreply.github.com>
2023-05-25 19:56:12 +0530

    Trying accessing sheets

--- exc.js
+++ exc.js
@@ -1,10 +1,12 @@
+let sheet_id='1lFCXlTomy4msjmlLTaeXNTM9yAylSEP8C2Wb1Pd55Ks'
+let sheet_title='Sheet1'
+let sheet_range='A1:B10'
 
-// //const XLSX=require("xlsx")
-// const workBook= XLSX.readFile("ala.xlsx")
-// const workSheet = workBook.Sheets("Table 1")
-// const channels=XLSX.utils.sheet_to_json(workSheet)
-// console.log(channels)
+let full_url = 'https://docs.google.com/spreadsheets/d/'+sheet_id+'/gviz/tq?sheet='+sheet_title+'&range='+sheet_range
 
-readXlsxFile("ala.xlsx").then((rows) => {
-    console.log(rows)
+fetch(full_url)
+.then(res => res.text())
+.then(rep => {
+    let data=JSON.parse(rep.substr(47).slice(0,-2))
+    console.log(data)
 })
